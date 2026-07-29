b623abbf4d0167db050525ec21dc4fddd62c7b97
DueiHowLightAction <59249807+DueiHowLightAction@users.noreply.github.com>
2026-07-29 10:22:06 +0000

    Deploying to gh-pages from @ DueiHowLightAction/DueiHowLightAction.github.io@eb7af01d2ab0eadda8b13c92de5c98db678609d1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "DueiHowLightAction/ChingEn-Kung.github.io",
+    "data-repo": "DueiHowLightAction/DueiHowLightAction.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
